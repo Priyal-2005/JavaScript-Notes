e689a51232cff4fd76a54f4dc7c862e5d9553cee
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-03-02 01:09:31 +0530

    last commit

--- callbacks.js
+++ callbacks.js
@@ -1,4 +1,17 @@
-/*
+// sync in js - code runs in a particular sequence of instructions each instruction waits for previous instruction to complete its execution
+
+// async in js - in sync, some instructions can get blocked and cause delay in the UI. Async will parallely run the code that is causing the delay while running the rest of the code
+console.log("one")
+console.log("two")
+
+setTimeout(() => {
+    console.log("hello")
+}, 4000)
+
+console.log("three")
+console.log("four")
+// one, two, three, four will be printed immediately. after four seconds, hello will be printed
+
 function sum(a, b) {
     return a + b;
 }
@@ -18,9 +31,7 @@ const hello = () => {
 }
 
 setTimeout(hello, 3000)
-*/
 
-// Callback hell --> Nested Callbacks
 function getData(dataId) {
     setTimeout(() => {
         console.log("data", dataId)
@@ -40,6 +51,7 @@ function getData(dataId, getNextData) {
     }, 2000)
 }
 
+// Callback hell --> Nested Callbacks
 getData(1, () => {
     getData(2, () => {
         getData(3, () => {
@@ -47,3 +59,5 @@ getData(1, () => {
         });
     });
 });
+
+// To solve callback hell, we have promises
