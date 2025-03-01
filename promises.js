a7f0f01c967448cf1f317dad125178a561bf637c
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-03-01 15:30:17 +0530

    second commit

--- promises.js
+++ promises.js
@@ -1,3 +1,5 @@
 // sync in js - code runs in a particular sequence of instructions each instruction waits for previous instruction to complete its execution
 
-// async in js - 
+// async in js - in sync, some instructions can get blocked and cause delay in the UI. Async will parallely run the code that is causing the delay while running the rest of the code
+
+setTimeout()
